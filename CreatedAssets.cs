269c188062fb7d55d6d96a1265ba5735d6672b97
G Bhanuteja <45899803+gbhanuteja23@users.noreply.github.com>
2020-09-06 17:37:48 +0530

    Add files via upload
    ARCore C# Scripts used

---
+++ CreatedAssets.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement;
+
+public class CreatedAssets : MonoBehaviour
+{
+    [SerializeField] private string _label;
+    [SerializeField] private string _assetName;
+
+    private List<GameObject> Assets { get; } = new List<GameObject>();
+
+    private void Start()
+    {
+        CreateAndWaitUntilCompleted();
+    }
+
+    private async Task CreateAndWaitUntilCompleted()
+    {
+        await CreateAddressablesLoader.InitAssets(_label, Assets);
+        await CreateAddressablesLoader.InitAssets(_assetName, Assets);
+
+        foreach (var asset in Assets)
+        {
+            //OBJS IS NOW LOADED
+            Debug.Log(asset.name);
+        }
+
+        //To destroy the loaded Addressable Asset
+        //await Task.Delay(2000);
+        //ClearAsset(Assets[0]);
+    }
+
+    private void ClearAsset(GameObject go)
+    {
+        Addressables.Release(go);
+    }
+}
---
+++ Main_menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();  
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
